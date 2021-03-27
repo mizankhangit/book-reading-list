5e7bd999288c8198e6ea557bae9abf42edf1d98c
mizankhangit <mizanurmgt@gmail.com>
2021-03-27 12:27:45 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is a simple book reading list
+
+## Using react context and addReducer hook
+
+Live [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/contexts/BookContext.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { bookReducer } from "../reducer/bookReducer";
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
+
+  return (
+    <BookContext.Provider value={{ books, dispatch }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProvider;
